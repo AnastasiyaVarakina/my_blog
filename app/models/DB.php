9f1572203ec1f32873125ca5ce2b1d3ad1147e78
AnastasiyaVarakina <varakina2005gvfg@mail.ru>
2026-03-30 10:38:56 +0300

    комментарии в подключениях и бд

--- app/models/DB.php
+++ app/models/DB.php
@@ -1,24 +1,34 @@
 <?
 
+// это паттерн, который гарантирует что у класса есть всего один экземпляр, 
+// и предоставляет к экземпляру глобальную точку доступа 
+// Например подключение к БД, достаточно одного подключения.
+
+
 class DB
 {
     private $conn;
+
+    // подготовленный запрос к бд
     private PDOStatement $stmt;
 
-    public static $instance = NULL;
+    public static $instance = NULL;//хранилище единственного экземпляра
 
-    private function __construct() {}
+    private function __construct() {}//делаем приватный, чтобы никто не мог создать
+    // экземпляр через New
 
+    // запрет клонирования
     // private function __clone()
     // {
     // }
 
-
+    // запрет десериализации(восстановления из сериализованной строки)
     // private function __wakeup() {
 
     // }
 
 
+    // подлкючение к
     public function getConnection($db_config) {
                 try {
             // DSN строка которая описывает к какой бд мы подключаемся
@@ -37,6 +47,8 @@ class DB
         };
     }
 
+    // если хранилище пустое, то создаем экземпляр, второй раз обратиться не сможем
+    // тк хранилище уже не нулевое
     public static function getInstance() {
         if(self::$instance === NULL) {
             self::$instance = new self();
