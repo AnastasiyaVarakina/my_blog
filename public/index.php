f9c806351f0f2f054c4613c626d5eebf813d22e4
AnastasiyaVarakina <varakina2005gvfg@mail.ru>
2026-04-01 20:03:50 +0300

    пример кукиса

--- public/index.php
+++ public/index.php
@@ -1,4 +1,14 @@
 <?php
+setcookie(
+    'lang', 
+    'ru', 
+    ['expires' => time() + 3600*24*30,//время на которое сохраняем этот кукис
+    'path' => '/',//путь проходит через все файлы
+    'secure' => true,//передача только по защищенному запросы
+    'httponly' => true,
+    'samesite' => 'Lax'
+    ]);
+
 
 require_once(dirname(__DIR__).'/config/config.php');
 
@@ -15,5 +25,4 @@ $db = DB::getInstance()->getConnection($db_config);
 // первый вызов он создает объект, дальше он уже будет просто возвращать существующий объект
 require_once MODELS."/router.php";
 // роутер должен стоять всегда в конце
-
 
